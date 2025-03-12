7fd4090feb238afc245e7432c1f6fbf2056f6064
Jan Scholz <jan.scholz@gmail.com>
2025-03-12 13:56:16 +0100

    try concept in a test

--- tests/test_versioning.py
+++ tests/test_versioning.py
@@ -6,3 +6,12 @@ def test_version():
 def test_reno_import_available():
     assert metadata.version("reno")
     from reno_structured_output import reno
+
+def test_reno():
+    from reno_structured_output import reno
+    from reno import lister, loader, scanner, config
+    conf = config.Config("./")
+    with scanner.Scanner(conf) as scnr:
+        relnotes = scnr.get_notes_by_version() # this should be almost it
+    assert relnotes
+    assert lister == "X"
