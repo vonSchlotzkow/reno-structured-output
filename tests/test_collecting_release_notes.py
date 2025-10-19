1545b98b9de7b072c92a2b7dd18a533a1311ec9a
Jan Scholz <jan.scholz@gmail.com>
2025-10-19 11:53:52 +0200

    fixup! hierarchies in output

--- tests/test_collecting_release_notes.py
+++ tests/test_collecting_release_notes.py
@@ -4,4 +4,4 @@ from reno_structured_output import collect_notes
 def test_collect_notes_as_python_structure():
     notes = collect_notes()
     assert isinstance(notes, dict)
-    assert "v0.0.2" in list(notes.keys())
+    assert "v0.0.2" in list(notes["relnotes"])
